849e59197e719c51b9f1cb66120aa4c502cbc339
umamahesh967 <mahesh.babu075@gmail.com>
2017-12-03 15:50:44 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use an official  runtime as a parent image
+FROM java:8-jre
+#FROM  docker/compose:1.17.1
+#FROM maven
+#FROM docker:17.11.0-ce
+
+# Set the working directory to /app
+WORKDIR /workflowte
+
+# Copy the current directory contents into the container at /app
+ADD . /workflowte
+
+# Make port 80 available to the world outside this container
+EXPOSE 8080
+EXPOSE 8122
+
+ENV HOMER="Hiiiiiiiiiiiiiiii sekhar"
+#RUN mvn clean package
+
+# Run app.py when the container launches
+CMD ["java","-jar","target/hello-springboot-1.3.5.RELEASE.jar"]
---
+++ src/main/java/com/stackroute/deploymentdashboard/HelloController.java
@@ -0,0 +1,28 @@
+package com.stackroute.deploymentdashboard;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+        @Value("${server.port}")
+        private String port_no;
+
+
+        @Value("${app.prop1}")
+        private String project_name;
+
+
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    	model.addAttribute("project_name", project_name);
+    	model.addAttribute("port_no", port_no);	
+    	return "view";
+    }
+}
+
+
